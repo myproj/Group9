a8ac10affafbe8d81830eb1a19b9dbf15f82567e
Yogendra Kumar Dhiwar <yogendrakumardhiwar786@gmail.com>
2016-08-29 16:18:34 +0530

    Negative check added

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,27 @@
 #include "functions.h"
+#include <iostream>
 
-int factorial(int n){
-    if(n!=1){
+using namespace std;
+
+int factorial1(int  n){
+	if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
+
+}
+
+
+int factorial(int n){
+	int k;
+	if( n < 0){
+		cout<<"Negative Number not allowed : Enter positve number\n";
+		cin>>k;
+		while(k<0){
+			cout<<"Negative Number not allowed : Enter positve number\n";
+			cin>>k;
+		}
+		n=k;
+	}
+    return factorial1(n);
 }
